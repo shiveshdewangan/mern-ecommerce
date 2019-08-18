aa3efeaed73e1240cd0d55eed3891f704b394c26
Shivesh Dewangan <shivesh.dewangan@gmail.com>
2019-08-18 14:00:00 +1000

    implemented basic controller

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("hello from node hey");
-});
+const { sayHi } = require("../controllers/user");
+
+router.get("/", sayHi);
 
 module.exports = router;
