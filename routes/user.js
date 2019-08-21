045018dd32118c8abfcb0ef7329a5a79bb92eee6
Shivesh Dewangan <shivesh.dewangan@macquarie.com>
2019-08-21 17:07:24 +1000

    added more middlewares - isAuth and isAdmin

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { requireSignIn } = require("../controllers/auth");
+const { requireSignIn, isAdmin, isAuth } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
-router.get("/secret/:userId", requireSignIn, (req, res) => {
+router.get("/secret/:userId", requireSignIn, isAuth, isAdmin, (req, res) => {
   res.json({
     user: req.profile
   });
